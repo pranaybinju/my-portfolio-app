a405b3db838b94fa6191113775cdad1a3c1530b2
PKB2810 <pranay@kiprosh.com>
2022-08-07 22:13:08 +0530

    added CI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Main from "./components/Main"
 import "./App.css"
 
 function App() {
-   return (
-      <div className="App">
-         <Main></Main>
-      </div>
-   )
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  )
 }
 
 export default App
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,112 +1,110 @@
 const Info = () => {
-   return (
-      <div
-         style={{
-            display: "flex",
-            flex: 1,
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginBottom: "2rem",
-         }}
-      >
-         <img
-            alt="my-img"
-            src="ash.jpeg"
-            style={{
-               width: 300,
-               height: 300,
-               borderRadius: "50%",
-               objectFit: "contain",
-               flex: 1,
-            }}
-         ></img>
-         <div style={{ flex: 1 }}>
-            <h1 style={{ fontSize: "3rem", margin: 0 }}>
-               {"Hi, I'm Ash Ketchum"}
-            </h1>
-            <p style={{ fontSize: "2rem", margin: 0 }}>
-               {"Pokemon Champion and Trainer from Japan"}
-            </p>
-         </div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "2rem",
+      }}
+    >
+      <img
+        alt="my-img"
+        src="ash.jpeg"
+        style={{
+          width: 300,
+          height: 300,
+          borderRadius: "50%",
+          objectFit: "contain",
+          flex: 1,
+        }}
+      ></img>
+      <div style={{ flex: 1 }}>
+        <h1 style={{ fontSize: "3rem", margin: 0 }}>{"Hi, I'm Ash Ketchum"}</h1>
+        <p style={{ fontSize: "2rem", margin: 0 }}>
+          {"Pokemon Champion and Trainer from Japan"}
+        </p>
       </div>
-   )
+    </div>
+  )
 }
 
 const Skills = () => {
-   return (
-      <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
-         <h1
-            style={{
-               fontSize: "3rem",
-               margin: 0,
-               textAlign: "center",
+  return (
+    <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+      <h1
+        style={{
+          fontSize: "3rem",
+          margin: 0,
+          textAlign: "center",
 
-               marginBottom: "1rem",
-            }}
-         >
-            {"Skills"}
-         </h1>
-         <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
-            {"Playing with Pikachu"}
-         </p>
-         <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
-            {"Training Pokemons"}
-         </p>
-         <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
-            {"Defeating Team Rocket"}
-         </p>
-      </div>
-   )
+          marginBottom: "1rem",
+        }}
+      >
+        {"Skills"}
+      </h1>
+      <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
+        {"Playing with Pikachu"}
+      </p>
+      <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
+        {"Training Pokemons"}
+      </p>
+      <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
+        {"Defeating Team Rocket"}
+      </p>
+    </div>
+  )
 }
 
 const Contact = () => {
-   return (
-      <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
-         <h1 style={{ fontSize: "3rem", margin: 0, textAlign: "center" }}>
-            {"Contact"}
-         </h1>
-         <p style={{ fontSize: "1.2rem" }}>{"ash.ketchum@pokemail.com"}</p>
-      </div>
-   )
+  return (
+    <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+      <h1 style={{ fontSize: "3rem", margin: 0, textAlign: "center" }}>
+        {"Contact"}
+      </h1>
+      <p style={{ fontSize: "1.2rem" }}>{"ash.ketchum@pokemail.com"}</p>
+    </div>
+  )
 }
 
 const Achievements = () => {
-   return (
-      <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
-         <h1
-            style={{
-               fontSize: "3rem",
-               margin: 0,
-               marginBottom: "1rem",
-               textAlign: "center",
-            }}
-         >
-            {"Achievements"}
-         </h1>
-         <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
-            {"Champion of the Orange League"}
-         </p>
-         <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
-            {"Champion of the Manalo Conference"}
-         </p>
-         <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
-            {"Pokémon Champion of the Orange Islands"}
-         </p>
-      </div>
-   )
+  return (
+    <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+      <h1
+        style={{
+          fontSize: "3rem",
+          margin: 0,
+          marginBottom: "1rem",
+          textAlign: "center",
+        }}
+      >
+        {"Achievements"}
+      </h1>
+      <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
+        {"Champion of the Orange League"}
+      </p>
+      <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
+        {"Champion of the Manalo Conference"}
+      </p>
+      <p style={{ fontSize: "1.2rem", margin: 0, marginBottom: "1rem" }}>
+        {"Pokémon Champion of the Orange Islands"}
+      </p>
+    </div>
+  )
 }
 
 const Main = () => {
-   return (
-      <div style={{ padding: "2rem" }}>
-         <Info />
-         <div style={{ display: "flex" }}>
-            <Skills />
-            <Achievements />
-            <Contact />
-         </div>
+  return (
+    <div style={{ padding: "2rem" }}>
+      <Info />
+      <div style={{ display: "flex" }}>
+        <Skills />
+        <Achievements />
+        <Contact />
       </div>
-   )
+    </div>
+  )
 }
 
 export default Main
