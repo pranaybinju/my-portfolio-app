9fcc6dd32f3744405113ac8d47edda5dee71c460
PKB2810 <pranay@kiprosh.com>
2022-08-08 19:55:39 +0530

    update tests, script, add gh-workflow

--- src/tests/Info.test.js
+++ src/tests/Info.test.js
@@ -1,4 +1,4 @@
-import { render, toHaveText } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import "@testing-library/jest-dom"
 
 import Info from "../components/Info"
